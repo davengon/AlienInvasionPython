d82dc230b4319e6dae6e80d56f9b757a9a7eba67
David <davidgonzalez5910@gmail.com>
2026-07-27 20:01:55 -0500

    Adjusting frames with Clock

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,6 +10,7 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        self.clock = pygame.time.Clock()
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -21,6 +22,7 @@ class AlienInvasion:
 
             #Make the most recently drawn screen visible
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     #Make a game instance, and run the game.
